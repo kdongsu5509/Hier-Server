57d786cca8bd6caf2e9e9ef57bb05a746b3e359c
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-08-13 19:28:02 +0900

    refact : delete unnecessary logic

--- src/main/java/com/dt/find_restaurant/global/config/scheduler/SchedulerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dt.find_restaurant.global.config.scheduler;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulerConfig {
-}
