5eb4c58fab14b3996fd5f8d5e3d4307da655b81f
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-08-29 17:11:17 +0900

    hotfix/ fix falsch annotation

--- src/main/java/com/dt/find_restaurant/pin/dto/PinRequest.java
+++ src/main/java/com/dt/find_restaurant/pin/dto/PinRequest.java
@@ -3,14 +3,14 @@ package com.dt.find_restaurant.pin.dto;
 import com.dt.find_restaurant.pin.domain.Address;
 import com.dt.find_restaurant.pin.domain.Pin;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import org.hibernate.validator.constraints.Length;
 
 public record PinRequest(
         @NotNull
         String restaurantName,
-        @NotNull @Min(value = 30, message = "최소 30자 이상 입력해주세요.")
+        @NotNull @Length(min = 30, message = "최소 30자 이상 입력해주세요.")
         String text,
         @NotNull
         String mapUrl,
