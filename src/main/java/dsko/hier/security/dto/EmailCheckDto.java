9f2cf1485e1c9afb89ce57ff49d061b7e175b520
kdongsu5509 <kod66170@gmail.com>
2025-09-22 19:49:45 +0900

    refact/ add notnull annotaion

--- src/main/java/dsko/hier/security/dto/EmailCheckDto.java
+++ src/main/java/dsko/hier/security/dto/EmailCheckDto.java
@@ -1,8 +1,10 @@
 package dsko.hier.security.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 public record EmailCheckDto(
+        @NotNull(message = "이메일은 필수 입력 값입니다.")
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         String email
 ) {
